7102b2ad2993452e0c8e863c65f1ff3666e3f725
Anezka Kirzten <anezka.tan@obf.ateneo.edu>
2023-06-23 13:25:34 +0800

    2nd commit

---
+++ ipa-2.py
@@ -0,0 +1,305 @@
+'''Individual Programming Assignment 2
+
+70 points
+
+This assignment will develop your proficiency with Python's control flows.
+'''
+
+def shift_letter(letter, shift):
+    '''Shift Letter.
+    5 points.
+
+    Shift a letter right by the given number.
+    Wrap the letter around if it reaches the end of the alphabet.
+
+    Examples:
+    shift_letter("A", 0) -> "A"
+    shift_letter("A", 2) -> "C"
+    shift_letter("Z", 1) -> "A"
+    shift_letter("X", 5) -> "C"
+    shift_letter(" ", _) -> " "
+
+    *Note: the single underscore `_` is used to acknowledge the presence
+        of a value without caring about its contents.
+
+    Parameters
+    ----------
+    letter: str
+        a single uppercase English letter, or a space.
+    shift: int
+        the number by which to shift the letter.
+
+    Returns
+    -------
+    str
+        the letter, shifted appropriately, if a letter.
+        a single space if the original letter was a space.
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+#def shift_letter(letter,shift):
+    shift=int(shift)
+    numerical=ord(letter)
+    order=shift+numerical
+    if numerical<=90 and numerical>65:
+        letter=chr(order-26)
+    elif order<90 and order>=65:
+        letter=chr(order)
+    elif letter=='_':
+        letter=' '
+    else:
+        letter='Error'
+    print(letter)
+
+shift_letter(',',2)
+
+def caesar_cipher(message, shift):
+    '''Caesar Cipher.
+    10 points.
+
+    Apply a shift number to a string of uppercase English letters and spaces.
+
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and spaces.
+    shift: int
+        the number by which to shift the letters.
+
+    Returns
+    -------
+    str
+        the message, shifted appropriately.
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+#def caesar_cipher(message, shift):
+    for x in message:
+            shift=int(shift)
+            numerical=ord(x)
+            order=shift+numerical
+            if numerical<=90 and order>90:
+                x=chr(order-26)
+            elif order<90 and order>=65:
+                x=chr(order)
+            elif x=='_':
+                x=' '
+            else:
+                x='Error'
+
+            print(x, end="")
+
+caesar_cipher('HELLO',2)
+
+def shift_by_letter(letter, letter_shift):
+    '''Shift By Letter.
+    10 points.
+
+    Shift a letter to the right using the number equivalent of another letter.
+    The shift letter is any letter from A to Z, where A represents 0, B represents 1,
+        ..., Z represents 25.
+
+    Examples:
+    shift_by_letter("A", "A") -> "A"
+    shift_by_letter("A", "C") -> "C"
+    shift_by_letter("B", "K") -> "L"
+    shift_by_letter(" ", _) -> " "
+
+    Parameters
+    ----------
+    letter: str
+        a single uppercase English letter, or a space.
+    letter_shift: str
+        a single uppercase English letter.
+
+    Returns
+    -------
+    str
+        the letter, shifted appropriately.
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    shift=ord(letter_shift)
+    numerical=ord(letter)
+    if shift<=90 and shift>=65 and numerical<=90 and numerical>=65:
+        new=chr(numerical+(shift-65))
+    elif letter==' ' and letter_shift=='_':
+        new = ' '
+    else:
+        new = 'Error.'
+    print(new)
+
+shift_by_letter('B','K')
+
+def vigenere_cipher(message, key):
+    '''Vigenere Cipher.
+    15 points.
+
+    Encrypts a message using a keyphrase instead of a static number.
+    Every letter in the message is shifted by the number represented by the
+        respective letter in the key.
+    Spaces should be ignored.
+
+    Example:
+    vigenere_cipher("A C", "KEY") -> "K A"
+
+    If needed, the keyphrase is extended to match the length of the key.
+        If the key is "KEY" and the message is "LONGTEXT",
+        the key will be extended to be "KEYKEYKE".
+
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and spaces.
+    key: str
+        a string of uppercase English letters. Will never be longer than the message.
+        Will never contain spaces.
+
+    Returns
+    -------
+    str
+        the message, shifted appropriately.
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+#def vigenere_cipher(message, key):
+    add=0
+    while len(key) != len(message):
+        key=key+key[add]
+        add+=1
+    else:      
+        message = list(message)
+        key = list(key)
+    
+    for x in message:
+        z=message.index(x)
+        x=ord(x)
+        if x >= 65 and x<=90:
+            y=key[z]
+            y=ord(y)
+            if (x+y-65) >= 65 and (x+y-65)<=90:
+                y=x+y-65
+                letter = chr(y)
+                print(letter,end="")
+            elif (x+y-65)>90 and (x+y-65)<=116:
+                y=x+y-65-26
+                letter=chr(y)
+                print(letter,end="")
+        elif x==32:
+            y=' '
+            print(y,end="")
+            
+        else:
+            y='Error'
+            print(y,end="")
+    
+
+vigenere_cipher ('CATRIONA', 'DOG')
+
+def scytale_cipher(message, shift):
+    '''Scytale Cipher.
+    15 points.
+
+    Encrypts a message by simulating a scytale cipher.
+
+    A scytale is a cylinder around which you can wrap a long strip of
+        parchment that contained a string of apparent gibberish. The parchment,
+        when read using the scytale, would reveal a message due to every nth
+        letter now appearing next to each other, revealing a proper message.
+    This encryption method is obsolete and should never be used to encrypt
+        data in production settings.
+
+    You may read more about the method here:
+        https://en.wikipedia.org/wiki/Scytale
+
+    You may follow this algorithm to implement a scytale-style cipher:
+    1. Take a message to be encoded and a "shift" number.
+        For this example, we will use "INFORMATION_AGE" as
+        the message and 3 as the shift.
+    2. Check if the length of the message is a multiple of
+        the shift. If it is not, add additional underscores
+        to the end of the message until it is.
+        In this example, "INFORMATION_AGE" is already a multiple of 3,
+        so we will leave it alone.
+    3. This is the tricky part. Construct the encoded message.
+        For each index i in the encoded message, use the character at the index
+        (i // shift) + (len(message) // shift) * (i % shift) of the raw message.
+        If this number doesn't make sense, you can play around with the cipher at
+         https://dencode.com/en/cipher/scytale to try to understand it.
+    4. Return the encoded message. In this case,
+        the output should be "IMNNA_FTAOIGROE".
+
+    Example:
+    scytale_cipher("INFORMATION_AGE", 3) -> "IMNNA_FTAOIGROE"
+    scytale_cipher("INFORMATION_AGE", 4) -> "IRIANMOGFANEOT__"
+    scytale_cipher("ALGORITHMS_ARE_IMPORTANT", 8) -> "AOTSRIOALRH_EMRNGIMA_PTT"
+
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and underscores (underscores represent spaces)
+    shift: int
+        a positive int that does not exceed the length of message
+
+    Returns
+    -------
+    str
+        the encoded message
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+#def scytale_cipher(message, shift):
+    while len(message)%shift!=0:
+        message=message+'_'
+    else:
+        #print(message)
+        num=int(len(message)/shift)
+        #print(num)
+        
+        for z in range (0, num):
+            for x in range (1,shift+1):
+                placement=x*num-num+z
+                y=message[placement]
+                print(y, end="")
+        
+            print('',end="")
+        else:
+            print ()
+
+scytale_cipher('ALGORITHMS_ARE_IMPORTANT', 8)
+
+def scytale_decipher(message, shift):
+    '''Scytale De-cipher.
+    15 points.
+
+    Decrypts a message that was originally encrypted with the `scytale_cipher` function above.
+
+    Example:
+    scytale_decipher("IMNNA_FTAOIGROE", 3) -> "INFORMATION_AGE"
+    scytale_decipher("AOTSRIOALRH_EMRNGIMA_PTT", 8) -> "ALGORITHMS_ARE_IMPORTANT"
+    scytale_decipher("IRIANMOGFANEOT__", 4) -> "INFORMATION_AGE_"
+
+    There is no further brief for this number.
+
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and underscores (underscores represent spaces)
+    shift: int
+        a positive int that does not exceed the length of message
+
+    Returns
+    -------
+    str
+        the decoded message
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+#def scytale_decipher(message, shift):
+    num=int(len(message)/shift)
+    
+    for z in range(0,shift):
+        for x in range(1,num+1):
+            y=x*shift-shift+z
+            print(message[y],end="")
+
