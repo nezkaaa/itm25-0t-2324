36b8a61b0fb160698545fd31908ab5cb3e9f9296
nezkaaa <135968980+nezkaaa@users.noreply.github.com>
2023-07-03 10:41:34 +0800

    July 03, 2023

--- ipa-3.py
+++ ipa-3.py
@@ -191,54 +191,6 @@ def tic_tac_toe(board):
         return('NO WINNER')
 
                         
-                        
-                        
-                        
-
-
-board1 = [
-['X','X','O'],
-['O','X','O'],
-['O','','X'],
-]
-
-board2 = [
-['X','X','O'],
-['O','X','O'],
-['','O','X'],
-]
-
-board3 = [
-['O','X','O'],
-['','O','X'],
-['X','X','O'],
-]
-
-board4 = [
-['X','X','X'],
-['O','X','O'],
-['O','','O'],
-]
-
-board5 = [
-['X','X','O'],
-['O','X','O'],
-['X','','O'],
-]
-
-board6 = [
-['X','X','O'],
-['O','X','O'],
-['X','',''],
-]
-
-board7 = [
-['X','X','O',''],
-['O','X','O','O'],
-['X','','','O'],
-['O','X','','']
-]
-
 def eta(first_stop, second_stop, route_map):
     '''ETA.
     25 points.
@@ -300,14 +252,5 @@ def eta(first_stop, second_stop, route_map):
     else:
         return(mins)
 
-legs = {
-    ('a1', 'a2'): {
-        'travel_time_mins': 10
-    },
-    ('a2', 'b1'): {
-        'travel_time_mins': 10230
-    },
-    ('b1', 'a1'): {
-        'travel_time_mins': 1
-}}
+
 
